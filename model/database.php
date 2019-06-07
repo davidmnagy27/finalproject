ff224e236339481aa7a74986a51e8f16e5efed5a
Trang Luu <tluu9@mail.greenriver.edu>
2019-06-07 15:37:53 -0400

    Added model

---
+++ model/database.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ CREATE TABLE customer (
+customer_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+car_id int NOT NULL,
+first_name varchar(40)NOT NULL,
+last_name varchar(40)NOT NULL,
+email varchar(255) NOT NULL,
+phone varchar(30) NOT NULL,
+pick_up datetime NOT NULL,
+drop_off datetime NOT NULL,
+age int(11) NOT NULL,
+FOREIGN KEY (car_id) REFERENCES car(car_id)
+);
+
+INSERT INTO car(name, seat,speed) VALUES
+('Audi R8 V10', '2 seats', '2.9 seconds'),
+('BMW I8 Coupe','2 seats', '3.8 seconds'),
+('Ferrari 458 Italia','2 seats','3.0 seconds'),
+('Lamborghini Aventador LP400-4','2 seats','2.8 seconds'),
+('Lamborghini Huracan LP610-4','2 seats','2.5 seconds'),
+('McLaren 650S','2 seats','3.0 seconds');
+ */
